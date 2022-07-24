e31f16ed555463164fbeca34b9f9daabaeaea276
Victor Ezea <victorvikson@gmail.com>
2022-07-24 04:10:44 +0100

    store modification

--- App.js
+++ App.js
@@ -3,18 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./src/navigation/Navigation";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-// import { store } from "./src/Redux/store";
-import cartReducer from "./src/Redux/CartSlice";
-import productReducer, { fetchProducts } from "./src/Redux/ProductSlice";
-
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    product: productReducer,
-  },
-});
-
-store.dispatch(fetchProducts());
+import store from "./src/Redux/store";
 
 export default function App() {
   return (
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
 import productReducer, { fetchProducts } from "./ProductSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: productReducer,
@@ -10,3 +10,5 @@ export const store = configureStore({
 });
 
 store.dispatch(fetchProducts());
+
+export default store;
